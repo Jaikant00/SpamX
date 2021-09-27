df46a291389e64a2a2d0d1eb9c4f90014987603d
Jaikant00 <91428708+Jaikant00@users.noreply.github.com>
2021-09-27 20:45:20 +0530

    Update and rename TBomb.sh to SpamX.sh

--- SpamX.sh
+++ SpamX.sh
@@ -27,15 +27,15 @@ banner() {
     clear
     echo -e "\e[1;31m"
     if ! [ -x "$(command -v figlet)" ]; then
-        echo 'Introducing TBomb'
+        echo 'Introducing SpamX'
     else
-        figlet TBomb
+        figlet SpamX
     fi
     if ! [ -x "$(command -v toilet)" ]; then
         echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSpeedX \e[0m"
     else
         echo -e "\e[1;34mCreated By \e[1;34m"
-        toilet -f mono12 -F border SpeedX
+        toilet -f mono12 -F border Jaikant00
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
     echo -e "\e[1;32m           Telegram: https://t.me/TBombChat \e[0m"
